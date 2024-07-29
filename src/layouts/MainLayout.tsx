4e6aa3aec28bd18b9cae06c8e71cda4405093e59
rijorajan585 <rijorajan585@gmail.com>
2024-07-29 10:24:51 +0530

    access to home layout of unauthorized person issue fix

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,11 +3,15 @@ import Header from "../components/Header";
 import SideMenu from "../components/SideMenu";
 import Footer from "../components/Footer";
 import { useLocation } from "react-router-dom";
+import { useAuth } from "../services/auth/AuthContext";
 
 const MainLayout: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
   const location = useLocation();
+  const { isAuthenticated } = useAuth();
   const showLayout =
-    location.pathname !== "/login" && location.pathname !== "/signup";
+    location.pathname !== "/login" &&
+    location.pathname !== "/signup" &&
+    isAuthenticated;
   return (
     <div className="flex flex-col min-h-screen">
       {showLayout && <Header />}
